96cb026c2d836bd9702093fc1d6aa7e874e3df33
n3rding <16728336+jebbett@users.noreply.github.com>
2025-03-27 10:10:16 +0000

    Add files via upload

--- content.js
+++ content.js
@@ -1,8 +1,23 @@
-document.addEventListener('DOMContentLoaded', function () {
+function setDropdownValue() {
   const dropdown = document.getElementById('native-select-option');
   if (dropdown) {
     dropdown.value = 'isAuthorHidden';
     const event = new Event('change');
     dropdown.dispatchEvent(event);
   }
+}
+
+// Create an observer instance linked to the callback function
+const observer = new MutationObserver((mutationsList, observer) => {
+  for (const mutation of mutationsList) {
+    if (mutation.type === 'childList') {
+      setDropdownValue();
+    }
+  }
 });
+
+// Start observing the target node for configured mutations
+observer.observe(document.body, { childList: true, subtree: true });
+
+// Initially set the dropdown value in case the element is already present
+setDropdownValue();
